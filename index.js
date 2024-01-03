b992c6e8cbf981ff9c1279abbb59adcb89dfebb5
Tiffany-Zhu5303 <Tiffany2016d@gmail.com>
2024-01-02 23:14:19 -0500

    Challenge 4 but game cards are not displayed evenly like expected

--- index.js
+++ index.js
@@ -29,27 +29,29 @@ const gamesContainer = document.getElementById("games-container");
 function addGamesToPage(games) {
 
     // loop over each item in the data
-
-
+        for (let i = 0; i < games.length; i++){
+            console.log(games[i]);
         // create a new div element, which will become the game card
-
+            let div = document.createElement('div');
 
         // add the class game-card to the list
-
+            div.classList.add("game-card");
 
         // set the inner HTML using a template literal to display some info 
         // about each game
         // TIP: if your images are not displaying, make sure there is space
         // between the end of the src attribute and the end of the tag ("/>")
-
+            div.innerHTML = `<image class="game-img" src=${games[i].img}></image><p><b>${games[i].name}</b></p><p>${games[i].description}</p><p>Backers: ${games[i].backers}</p>`;
 
         // append the game to the games-container
+            document.body.appendChild(div);
+        }
 
 }
 
 // call the function we just defined using the correct variable
 // later, we'll call this function using a different list of games
-
+addGamesToPage(GAMES_JSON);
 
 /*************************************************************************************
  * Challenge 4: Create the summary statistics at the top of the page displaying the
